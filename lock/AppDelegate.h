7db7c720b491fded57584578cb236c348c21909a
Doodleman360 <julislo123@gmail.com>
2016-10-26 16:27:41 -0600

    Initial Commit

---
+++ lock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lock
+//
+//  Created by Julien Sloan on 10/26/16.
+//  Copyright © 2016 Julien Sloan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lock
+//
+//  Created by Julien Sloan on 10/26/16.
+//  Copyright © 2016 Julien Sloan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
